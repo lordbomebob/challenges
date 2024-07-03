ac5957a9f835820d802fe0b33e8e497e89caad61
lordbomebob <lordbomebob@gmail.com>
2024-07-03 11:50:50 -0700

    +3 more challenges

--- challenges.js
+++ challenges.js
@@ -64,3 +64,142 @@ var sortArray = function(nums) {
     }
     return [...sortArray(left),pivot,...sortArray(right)]
 };
+//https://leetcode.com/problems/implement-queue-using-stacks/
+
+var MyQueue = function() {
+    this.q=[]
+    
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+    this.q.push(x)
+
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+    const val=this.q[0]
+    let x=0
+    while(this.q[x]){
+        this.q[x]=this.q[x+1]
+        x++
+    }
+    return val
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+    return this.q[0]
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+    return this.q.length===0
+};
+
+/** 
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
+//https://leetcode.com/problems/lru-cache/description/
+
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function(capacity) {
+    this.map= new Map()
+    this.capacity=capacity
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function(key) {
+    if(!this.map.has(key)){
+        return -1
+    }
+    const val= this.map.get(key)
+    this.map.delete(key)
+    this.map.set(key, val)
+    return val
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function(key, value) {
+    this.map.delete(key)
+    this.map.set(key, value)
+    if(this.map.size>this.capacity){
+        const firstItem= this.map.keys().next().value
+        this.map.delete(firstItem)
+    }
+};
+
+/** 
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
+
+https://leetcode.com/problems/implement-stack-using-queues/description/
+
+var MyStack = function() {
+    this.q=[]
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function(x) {
+    this.q.push(x)
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.pop = function() {
+    return this.q.pop()
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.top = function() {
+    return this.q[this.q.length-1]
+};
+
+/**
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function() {
+    return this.q.length===0
+};
+
+/** 
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
